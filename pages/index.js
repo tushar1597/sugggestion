f4a28dae9341234e296df10027518251e86248c6
tushar1597 <tusharsibal1597@gmail.com>
2021-03-31 10:01:11 +0530

    text area

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,114 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import suggestionStyles from '../css/custom/suggestion.module.css'
+import { useState, useContext } from 'react';
+
+const names = [
+  {name: "Tushar Sibal" , id: 1},
+  {name: "Aman Agarwal", id:2 },
+  {name: "Udit malhotra", id: 3},
+  {name: "Mohit agarwal", id: 4},
+  {name: "Ustaleen Kaur", id: 5},
+  {name: "Arun Singh", id: 6}
+]
 
 export default function Home() {
+  const [display,setDisplay] = useState(false);
+  const [filteredNames, setFilteredNames] = useState(names);
+  const [currentLi, setCurrentLi ] = useState(2);
+
+  const onInputChange = (e,from) => {
+    if(e.keyCode == 40 || e.keyCode == 38){
+      e.preventDefault();
+      console.log("Default..");
+    }
+    console.log(e.target.innerHTML);
+    return;
+    // let value = e.target.value;
+    // let split_value = value.split(' ')  
+    // console.log(split_value,split_value[split_value.length - 1]);
+    console.log(from,e.target.selectionStart,e.target.selectionEnd,e.keyCode,e.target.value);
+    // console.log(e.target.value);
+    console.log(e);
+    
+    let current_cursor = e.target.selectionStart;
+    let i = current_cursor;
+    let str = '';
+    while(true){
+      if(i==0){
+        console.log("Exiting");
+        setDisplay(false);
+        break;
+      }
+      if(value[i-1] === ' '){
+        console.log("Space Encountered");
+        setDisplay(false);
+        break;
+      }
+      if(value[i-1] === '@'){
+        if(str){
+          console.log("@ found==>> ",str);
+          setDisplay(true);
+        } else {
+          setDisplay(false);
+        }
+        break;
+      }
+      console.log(value[i-1]);
+      str = value[i-1] + str;
+      i--;
+      // break;
+    }
+    let elem = document.getElementById("sg-textarea");
+    console.log(elem.scrollHeight);
+  }
+  const onKeyDown = (e) => {
+    if(e.keyCode == 38){
+      e.preventDefault();
+      if(currentLi == 1){
+        setCurrentLi(filteredNames.length);
+      } else {
+        setCurrentLi(currentLi -1);
+      }
+      console.log("Default..");
+    } else if(e.keyCode == 40){
+      e.preventDefault();
+      if(currentLi == filteredNames.length){
+        setCurrentLi(1);
+      } else {
+        setCurrentLi(currentLi +1);
+      }
+      console.log("Default..");
+    }
+  }
+  console.log(filteredNames);
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <>
+    <div className={suggestionStyles["sg-container"]}>
+      <div>
+        { display ?
+          <div className={suggestionStyles["sg-box"]}>
+            <ul> 
+            {
+              filteredNames.map(({ name, id },index) =>{
+                return(
+                  <li className={suggestionStyles["sg-li"]} aria-selected={currentLi == (index + 1) ? true : false} key={"name_"+index}>{name}</li>
+                )
+                
+              })
+            }
+            </ul>
+          </div> : null
+        }
+      <div id="sg-textarea" contentEditable={true}
+      // onSelect={(e) => onInputChange(e,"OnSelect")} 
+      onKeyDown={(e) => onKeyDown(e,"OnKeyDown")}
+      onKeyUp={(e) => onInputChange(e,"OnKeyUp")} 
+      onMouseUp={(e) => onInputChange(e,"OnMouseUp")} 
+      // onMouseDown={(e) => onInputChange(e,"OnMouseDown")} 
+      className={suggestionStyles["sg-ta"]}>
+      </div>
+      </div>
     </div>
+    </>    
   )
 }
